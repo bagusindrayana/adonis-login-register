adaa0c4a64859382713a929b01d068e9f330fe5b
bagusindrayana <bagusindrayana@users.noreply.github.com>
2021-03-10 15:06:31 +0800

    first

---
+++ app/Controllers/Http/Auth/LoginController.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class LoginController {
+    index({ view }) {
+        return view.render('auth.login')
+    }
+
+    async check({ request, auth, session, response }) {
+
+    /**
+     * get data from form
+     */
+    const { email, password } = request.all()
+
+    /**
+     * attemp auth
+     */
+    await auth.attempt(email, password)
+
+        return response.route('dashboard')
+
+    }
+
+    async logout({ auth, response }) {
+        await auth.logout()
+        return response.route('welcome')
+    }
+}
+
+module.exports = LoginController
---
+++ start/routes.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome').as('welcome')
+
+Route.group(() => {
+    Route.get('register', 'Auth/RegisterController.index').as('register.index')
+    Route.post('register', 'Auth/RegisterController.store').as('register.store')
+
+    Route.get('login', 'Auth/LoginController.index').as('login.index')
+    Route.post('login', 'Auth/LoginController.check').as('login.check')
+}).middleware(['RedirectIfAuthenticated'])
+
+Route.group(() => {
+    Route.get('logout', 'Auth/LoginController.logout').as('logout')
+    Route.get('dashboard', 'DashboardController.index').as('dashboard')
+}).middleware(['Authenticate'])
+
+
